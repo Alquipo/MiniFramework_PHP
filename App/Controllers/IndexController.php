1be68b735e6a6ef1de4bfac65b0c8ccc8d60ea01
Alquipo Neto <alquiponeto@outlook.com.br>
2020-04-16 19:30:08 -0300

    conexao BD

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,17 +2,30 @@
     namespace App\Controllers;
     
     use MF\Controller\Action;
+    use App\Connection;
+    use App\Models\Produto;
 
     class IndexController extends Action{
 
         public function index(){
-            $this->view->dados = array('Sofa', 'cadeira', 'cama');
+           // $this->view->dados = array('Sofa', 'cadeira', 'cama');
+
+           //instancia da conexao
+            $conn = Connection::getDb();
+
+            //instancia o modelo
+            $produto = new Produto($conn);
+
+            $produtos = $produto->getProdutos();
+
+            $this->view->dados = $produtos;
+
             $this->render('index', 'layout2');
 
         }
 
         public function sobreNos(){
-            $this->view->dados = array('notebook', 'smartphone', 'celular');
+            //$this->view->dados = array('notebook', 'smartphone', 'celular');
             $this->render('sobreNos', 'layout1');
 
         }
